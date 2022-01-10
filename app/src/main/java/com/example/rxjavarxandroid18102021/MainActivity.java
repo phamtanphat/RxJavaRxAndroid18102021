8384f2143e02bda61fe5b3af6f24b8d6d8b7d140
phamtanphat <phatdroid94@gmail.com>
2022-01-10 18:48:35 +0700

    future

--- app/src/main/java/com/example/rxjavarxandroid18102021/MainActivity.java
+++ app/src/main/java/com/example/rxjavarxandroid18102021/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.rxjavarxandroid18102021;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,16 +25,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<String> arr = new ArrayList<>();
-        arr.add("Tèo");
-        arr.add("Tí");
-        arr.add("Tủn");
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-        Iterator<String> data =  arr.iterator();
 
-        while (data.hasNext()) {
-            Log.d("BBB", data.next());
+        Future<String> future1 = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                Thread.sleep(4000);
+                return "hello";
+            }
+        });
+
+        Future<String> future2 = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return "abc";
+            }
+        });
+        try {
+            Log.d("BBB",future1.get());
+            Log.d("BBB",future2.get());
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
     }
+
 }
